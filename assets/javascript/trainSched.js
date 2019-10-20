eb9ab9a87240260ff0e0cef7adf1cec52f0c4d7d
anvcodes <kpeace0720@gmail.com>
2019-10-19 20:26:19 -0400

    updates

--- assets/javascript/trainSched.js
+++ assets/javascript/trainSched.js
@@ -4,24 +4,4 @@
 
 
 
- $("#subBtn").on("click", function(event){
- event.preventDefault();
- var database = firebase.database();
  
-    var trainName= $("#trainName").val().trim();
-    var trainFrequency= $("#frequency").val().trim();
-    var trainDestination = $("#Destination").val().trim();
-    var firstTrainTime = $("#firstTrainTime").val().trim();
-
-
-    var newTrainInfo= {
-        train: trainName,
-        frequency: trainFrequency,
-        destination: trainDestination,
-        firstTrain: firstTrainTime,
-      }
-
-    database.ref().push(newTrainInfo);
-
-
- });
